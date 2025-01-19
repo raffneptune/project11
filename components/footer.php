16a2806cd4832f4bf11b98be0c4c34ad6713f0bd
Raffi <raffisaputra2410@gmail.com>
2025-01-19 13:48:20 +0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <a href="https://wa.me/6289635824169"><i class="fas fa-phone"></i> +62 896-3582-4169</a>
+         <a href="https://wa.me/6282112824744"><i class="fas fa-phone"></i> +62 821-1282-4744</a>
+         <a href="mailto:raffisaputra2410@gmail.com"><i class="fas fa-envelope"></i> raffisaputra2410@gmail.com</a>
+         <a href="mailto:kevinsebastianshmbng@gmail.com"><i class="fas fa-envelope"></i> kevinsebastianshmbng@gmail.com</a>
+         <a href="https://maps.app.goo.gl/qs2A4ksYj1ZxtQ2D7"><i class="fas fa-map-marker-alt"></i> JKT48 Theater</a>
+      </div>
+
+      <div class="box">
+         <a href="#home">home</a>
+         <a href="#about">about</a>
+         <a href="bookings.php">my bookings</a>
+         <a href="#reservation">reservation</a>
+         <a href="#gallery">gallery</a>
+         <a href="#contact">contact</a>
+         <a href="#reviews">reviews</a>
+      </div>
+
+      <div class="box">
+         <a href="#">facebook <i class="fab fa-facebook-f"></i></a>
+         <a href="#">twitter <i class="fab fa-twitter"></i></a>
+         <a href="https://www.instagram.com/raffneptune">instagram <i class="fab fa-instagram"></i></a>
+         <a href="#">linkedin <i class="fab fa-linkedin"></i></a>
+         <a href="#">youtube <i class="fab fa-youtube"></i></a>
+      </div>
+
+   </div>                  
+
+   <div class="credit">&copy; copyright @ 2025 by raffneptune | all rights reseved!</div>
+
+</section>
+
+<!-- footer section ends -->
